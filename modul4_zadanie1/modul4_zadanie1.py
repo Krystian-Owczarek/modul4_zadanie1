e1beb13642296d483c75b6caf5cf9b9cb2cd8ddc
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-28 14:44:52 +0200

    funkcja palindrom

--- modul4_zadanie1/modul4_zadanie1.py
+++ modul4_zadanie1/modul4_zadanie1.py
@@ -1,1 +1,9 @@
-print('Lets Begin')
+def is_palindrom(keyword):
+    rev_keyword = reversed(keyword)
+    if list(keyword) == list(rev_keyword):
+        print("to palindrom")
+    else:
+        print("nie jest palindromem")
+
+keyword = input('Słowo: ')
+is_palindrom(keyword)
