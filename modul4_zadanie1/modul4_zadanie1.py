564b3c78dd5e5107760ddb10ec90b067e39a6023
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-28 15:04:06 +0200

    Add Documentation

--- modul4_zadanie1/modul4_zadanie1.py
+++ modul4_zadanie1/modul4_zadanie1.py
@@ -1,6 +1,16 @@
 def is_palindrom(keyword):
+    '''
+    Funkcja sprawdza czy podane słowo jest palindromem.
+    Przyjmuje SŁOWO podane przez użytkownika jako zmienną.
+    Tworzy nową zmienną z odwróconą kolejnością SŁOWA.
+    Konwertuje stringi na listy, składające się z liter tworzących SŁOWO.
+    Przyrównuje obie listy i w zależności czy są identyczne wypisuje wynik.
+
+    '''
+
     rev_keyword = reversed(keyword)
     if list(keyword) == list(rev_keyword):
+        print(list(keyword))
         print("to palindrom")
     else:
         print("nie jest palindromem")
