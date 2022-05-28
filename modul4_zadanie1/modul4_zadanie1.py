eb2f0fbdbfe1e45b1dc5d11740246f850d905ef6
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-28 15:23:50 +0200

    Correcting case sensitiveness

--- modul4_zadanie1/modul4_zadanie1.py
+++ modul4_zadanie1/modul4_zadanie1.py
@@ -2,12 +2,13 @@ def is_palindrom(keyword):
     '''
     Funkcja sprawdza czy podane słowo jest palindromem.
     Przyjmuje SŁOWO podane przez użytkownika jako zmienną.
+    Przetwarza SŁOWO na składające się z małych liter.
     Tworzy nową zmienną z odwróconą kolejnością SŁOWA.
     Konwertuje stringi na listy, składające się z liter tworzących SŁOWO.
     Przyrównuje obie listy i w zależności czy są identyczne wypisuje wynik.
 
     '''
-
+    keyword = keyword.casefold()
     rev_keyword = reversed(keyword)
     if list(keyword) == list(rev_keyword):
         print(list(keyword))
